ac657761d09a5da491cbd80f6ed46868b2523133
medinaahmeti <medinaah1@gmail.com>
2018-12-20 13:55:05 +0100

    ndryshim

---
+++ javascript.js
@@ -0,0 +1,8 @@
+$(function () {
+     $("#flip").click(function(){
+
+ $("#panel").slideToggle("slow");//edhe e shfaq edhe e largon
+   //$("#panel").slideDown("slow");//nuk largon
+ $("#flip").text("Shiko me shume...");
+});
+   }) ;
