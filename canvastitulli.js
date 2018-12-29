0a0ffd4d35baa305d20362843bcd47771147900c
medinaahmeti <medinaah1@gmail.com>
2018-12-29 15:54:52 +0100

    ndryshim

--- canvastitulli.js
+++ canvastitulli.js
@@ -2,8 +2,8 @@
 window.onload = function() {
 var canvas1 = document.getElementById("titull");
 var ctx1=canvas1.getContext("2d");
-ctx1.font="34px fantasy";
-ctx1.fillStyle = "black";
-ctx1.textAlign = "center";
-ctx1.fillText(" 'Earth without art it's just eh!'", canvas1.width/2, canvas1.height/2);
+ctx1.font="24px fantasy";
+ctx1.fillStyle = "white";
+ctx1.textAlign = "left";
+ctx1.fillText("AAM", canvas1.width/2, canvas1.height/2);
 };
