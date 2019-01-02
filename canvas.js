ab59fe59ebc7c9c2d0df4d88ca3a43cc39688eae
medinaahmeti <medinaah1@gmail.com>
2019-01-01 22:14:42 -0800

    ndryshim

--- canvas.js
+++ canvas.js
@@ -2,7 +2,8 @@
 window.onload = function() {
     var canvas = document.getElementById("myCanvas");
     var ctx = canvas.getContext("2d");
-  
+
     var img = document.getElementById("scream");
-   ctx.drawImage(img, 10, 10);
+  // ctx.drawImage(img, 10, 10);
+ctx.drawImage(img,canvas.width/23, canvas.height/23);
 };
