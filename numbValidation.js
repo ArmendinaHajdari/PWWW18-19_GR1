3d4723abb8225eee255e2cc446153ccacd5b87c5
medinaahmeti <medinaah1@gmail.com>
2019-01-03 23:13:21 -0800

    ndryshim

--- numbValidation.js
+++ numbValidation.js
@@ -1,23 +1,20 @@
 function myFunction() {
   var x, text;//local
-
   // Get the value of the input field with id="numb"
   x = document.getElementById("numb").value;
 
   // If x is Not a Number or less than one or greater than 10
 try{
-    if (x<11 && x>0)
-    {alert("input valid");}
-  /*  else{alert('input jovalid');}*/
-  }
+        if (x<11 && x>0)
+        {alert("input valid");}
+        else{ message:('input jovalid');}
+    }
 
   catch(e){
-    alert("input jo valid");
-
-  }
+           alert(e.message);}
 finally {
-  alert('perfundim');
-}
+         alert('perfundim');}
+
 }
 
 /*  document.getElementById("demo").innerHTML = text;
