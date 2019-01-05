f5d44f0e446a38844eb5c891c788d25c73b4c51e
medinaahmeti <medinaah1@gmail.com>
2019-01-05 00:37:23 -0800

    ndryshim

--- numbValidation.js
+++ numbValidation.js
@@ -4,11 +4,11 @@ function myFunction() {
   message.innerHTML = "";
   x = document.getElementById("numb").value;
   try {
-    if(x == "")  throw "vendos nje numer brenda rangut";
-    if(isNaN(x)) throw "vendos nje numer brenda rangut";
+    if(x == "")  throw "Vendos nje numer brenda rangut";
+    if(isNaN(x)) throw "Vendos nje numer brenda rangut";
     x = Number(x);
-    if(x < 1)  throw "vendos nje numer brenda rangut";
-    if(x > 10)   throw "vendos nje numer brenda rangut";
+    if(x < 1)  throw "Vendos nje numer brenda rangut";
+    if(x > 11)   throw "Vendos nje numer brenda rangut";
   }
   catch(err) {
     message.innerHTML = "" + err;
