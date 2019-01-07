4de85cdfa4b367a08f0ff51e81ba91eaa9390e11
ArdianHoti18 <1ardianhoti@gmail.com>
2019-01-07 12:22:57 +0100

    ndryshim

--- cache.js
+++
@@ -1,8 +0,0 @@
-function onUpdateReady() {
-if(window.applicationCache.status === window.applicationCache.UPDATEREADY) {
-        		window.applicationCache.swapCache();
-        		location.reload();
-
-}
-}
-window.applicationCache.addEventListener('updateready', onUpdateReady);
