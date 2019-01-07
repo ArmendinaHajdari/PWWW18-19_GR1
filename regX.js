178a0efaf8f395f8252c6934da9d842b0beeea1c
ArdianHoti18 <1ardianhoti@gmail.com>
2019-01-07 18:14:19 +0100

    ndryshim

--- regX.js
+++ regX.js
@@ -1,10 +1,13 @@
-function allLetter(inputtext) {
+function allLetter() {
 
-var name = document.getElementById('name');
+var name = document.getElementById('name1');
 var filter = /^[A-Za-z]+$/;
 if (!filter.test(name.value)) {
        alert('Vendos vetem shkronja');
 
        return false;
      }
+
+     if (filter.test(name.value)) {
+       return true;}
     }
